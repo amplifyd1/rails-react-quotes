9b2c59ffd3148ebb5640dafc9e4b2e0e8093d448
wesley sprague <wesleysprague@yahoo.com>
2019-01-03 15:34:33 -0500

    added quotes model, seed date, show action, show jbuilder

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="react" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Quote.delete_all
+
+Quote.create!(
+  [{
+    text: "Have I dated a super model? Of course not. I'd look Ridiculous",
+    author: "Moby"
+  },
+  { text: "There may be people that have more talent than you, but theres no excuse for anyone to work harder than you do",
+  author: "Derrek Jeter"
+  },
+  { text: "Set your goals high, and don’t stop till you get there.",
+    author: "Bo Jackson"
+  },
+  { text: "The principle is competing against yourself. It’s about self-improvement, about being better than you were the day before",
+    author: "Steve Young"
+  },
+  { text: "Most talented players don’t always succeed. Some don’t even make the team. It’s more what’s inside.",
+    author: "Brett Favre"
+  }
+]
+)
+puts "They have spoken!"
